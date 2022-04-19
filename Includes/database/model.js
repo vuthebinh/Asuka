01ebb783a1b760dba27dadf5eac917a0619b2921
vuthebinh <77559484+vuthebinh@users.noreply.github.com>
2022-04-19 18:20:51 +0700

    Update model.js

--- Includes/database/model.js
+++ Includes/database/model.js
@@ -1,1 +1,10 @@
 
+const { Users, Threads, Currencies } = require("./models");
+
+module.exports.model = {
+	Users,
+	Threads,
+	Currencies
+};
+
+module.exports.use = (modelName) => this.model[`${modelName}`];
